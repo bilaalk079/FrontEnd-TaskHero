3c04f7146c0c3db63b96d1447a8c7bf5eb363542
bilaalk079 <kolawolebilaal13@gmail.com>
2025-06-09 18:43:56 +0100

    optimized mobile responsiveness

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -106,7 +106,7 @@ const Tasks = () => {
 
   return (
     <div className="w-full ">
-      <div className="mx-3 flex justify-between items-center lg:w-full sm:w-sm fixed top-0 mt-8">
+      <div className="mx-3 flex justify-between items-center p-2 w-full fixed top-0 mt-12">
         <motion.a
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
