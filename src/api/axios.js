dda776a1882ebe20e8802d304c3e090d8f1ccad7
bilaalk079 <kolawolebilaal13@gmail.com>
2025-06-09 12:57:38 +0100

    Made it ready for Production

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "../store/authStore";
 
 const api = axios.create({
-    baseURL:'http://localhost:5000/api',
+    baseURL:'https://backend-taskhero-1.onrender.com',
     withCredentials:true,
 })
 
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // replace with your backend port
+        target: 'https://backend-taskhero-1.onrender.com', // replace with your backend port
         changeOrigin: true,
         secure: false,
       },
